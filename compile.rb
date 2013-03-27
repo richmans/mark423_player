2257ab3ce8ad6f3e8f31078a22f0358f69267d32
Richard Bronkhorst <richardbronkhorst@hotmail.com>
2013-03-27 22:29:38 +0100

    initial project setup

---
+++ compile.rb
@@ -0,0 +1,21 @@
+def write_file(filename, output)
+  input = File.open(filename, 'r')
+  while line = input.gets
+    output.puts line
+  end
+  input.close
+end
+begin
+  Dir.mkdir("build")
+rescue Errno::EEXIST
+end
+output_file = File.open("build/mark423-player.js", 'w')
+puts "Writing jquery"
+write_file("lib/jquery-1.9.1.min.js", output_file)
+puts "Writing jplayer"
+write_file("lib/jquery.jplayer.min.js", output_file)
+puts "Writing playlist plugin"
+write_file("lib/jplayer.playlist.min.js", output_file)
+
+output_file.close
+puts "All done!"
