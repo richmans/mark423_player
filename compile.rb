62d960feb8d77a1a88b67f4216bc7f07fb7b0cb3
Richard Bronkhorst <richardbronkhorst@hotmail.com>
2013-03-31 23:06:27 +0200

    first steps towards implementing the single js build

--- compile.rb
+++ compile.rb
@@ -5,6 +5,18 @@ def write_file(filename, output)
   end
   input.close
 end
+
+def write_html_string(input_filename, output)
+  output.puts "mark423Interface = '' + "
+  input = File.open(input_filename, 'r')
+  while line = input.gets
+    line.gsub! '"', '\"'
+    output.puts "\"#{line.strip}\" + \n"
+  end
+  output.puts '"";'
+  input.close
+end
+
 begin
   Dir.mkdir("build")
 rescue Errno::EEXIST
@@ -14,8 +26,11 @@ puts "Writing jquery"
 write_file("lib/jquery-1.9.1.min.js", output_file)
 puts "Writing jplayer"
 write_file("lib/jquery.jplayer.min.js", output_file)
+puts "Writing html string"
+write_html_string("html/player.html", output_file)
 puts "Writing application js"
 write_file("lib/application.js", output_file)
-
 output_file.close
+
+# how about some minimization here?
 puts "All done!"
